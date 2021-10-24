0b14a3eee240d534a4ea3ab9d797667011e94791
Juarez Ribeiro <juarezribeiro@gmail.com>
2021-10-24 20:12:35 -0300

    Adicionando material table

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course } from './models/course';
+
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
+
+  courses: Course[] = [
+    {_id:'1', name: 'Angular', category:'Front'},
+    {_id:'2', name: 'React', category:'Front'},
+    {_id:'3', name: '.net 5', category:'Back'},
+    {_id:'4', name: 'Spring', category:'Back'},
+  ];
+
+  displayedColumns = ['name', 'category'];
 
   constructor() { }
 
